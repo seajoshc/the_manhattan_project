d6b7af2e20afd74b5ac7d3d7cc23f16ffe238661
Josh Campbell <joshcb@amazon.com>
2015-11-11 18:36:45 -0600

    adds method to check if a user is logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  # redirect user to login if they are not
+  def user_logged_in?
+    return if logged_in?
+    flash[:alert] = 'You must be logged in to do that!'
+    redirect_to request.referrer
+  end # user_logged_in?
 end
