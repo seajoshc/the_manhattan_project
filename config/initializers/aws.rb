748a2034c96df9a9b461ab6cc42a004c4d4ee658
Josh Campbell <joshcb@amazon.com>
2015-11-24 15:41:43 -0600

    do not load in test env

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,6 @@
-Aws.use_bundled_cert!
-Aws.config.update(region: ENV['AWS_REGION'])
-s3 = Aws::S3::Resource.new
-S3_BUCKET = s3.bucket(ENV['BUCKET_NAME'])
+unless Rails.env.test?
+  Aws.use_bundled_cert!
+  Aws.config.update(region: ENV['AWS_REGION'])
+  s3 = Aws::S3::Resource.new
+  S3_BUCKET = s3.bucket(ENV['BUCKET_NAME'])
+end
